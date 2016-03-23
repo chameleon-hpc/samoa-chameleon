8ae8d21890192d4a2fbe0e972748c1599d9fa095
Chaulio Ferreira <chaulio@hotmail.com>
2016-03-23 18:07:07 +0100

    SWE_SIMD: computed refinement/coarsening relationships between parent/children cells which will be needed later for implementing adaptivity

--- src/SWE/SWE_SIMD.f90
+++ src/SWE/SWE_SIMD.f90
@@ -28,6 +28,9 @@ MODULE SWE_SIMD
 		! one vector of 2x2 transformation matrices for each i_plotter_type, to avoid repeated computation of this
 		REAL (kind = GRID_SR), DIMENSION(_SWE_SIMD_NUM_EDGES_ALIGNMENT,2,2,-8:8) :: transform_matrices
 
+		! represents relationships between cells within a coarse patch and its two refined children
+		REAL (kind = GRID_SR), DIMENSION(_SWE_SIMD_ORDER_SQUARE,2) :: first_child, second_child
+
 		contains
 		
 		procedure, pass :: init => SWE_SIMD_geometry_init
@@ -36,6 +39,8 @@ MODULE SWE_SIMD
 		procedure, pass :: compute_ghost_edges => SWE_SIMD_compute_ghost_edges
 		procedure, pass :: compute_coords => SWE_SIMD_compute_coords
 		procedure, pass :: compute_transform => SWE_SIMD_compute_transform
+		procedure, pass :: compute_adaptivity => SWE_SIMD_compute_adaptivity
+		procedure, pass	:: cell_in_which_the_point_lies
 
 	END TYPE
 
@@ -74,6 +79,9 @@ MODULE SWE_SIMD
 		
 		! compute transformation matrices for the solver
 		call geom%compute_transform()
+
+		! compute relationships between parent cells and children cells
+		call geom%compute_adaptivity()
 	
 	END SUBROUTINE	
 
@@ -190,6 +198,7 @@ MODULE SWE_SIMD
 
 		integer											:: i, row, col
 		real (kind = GRID_SR), dimension(2,3)			:: r_points
+		real (kind = GRID_SR), dimension(2)				:: tmp
 		real (kind = GRID_SR)							:: d
 		
 		! length of each leg edge
@@ -207,6 +216,13 @@ MODULE SWE_SIMD
 			! copy coordinates to array
 			geom%coords(:,:,i) = r_points
 			
+			! make sure that all triangles are described in counterclockwise order
+			if (mod(col,2) == 1) then
+				tmp = geom%coords(:,1,i)
+				geom%coords(:,1,i) = geom%coords(:,2,i)
+				geom%coords(:,2,i) = tmp
+			end if
+
 			!compute next points
 			col = col + 1
 			if (col == 2*row) then
@@ -257,10 +273,113 @@ MODULE SWE_SIMD
 				end do
             end if
         end do
-		
-		
-		
 	END SUBROUTINE
+	
+    ! finds out relationships between cells on refinement and coarsening operations
+	SUBROUTINE SWE_SIMD_compute_adaptivity(geom)
+		IMPLICIT NONE
+		CLASS(t_SWE_SIMD_geometry), INTENT(INOUT) 		:: geom
+		integer											:: i, j
+		real (kind = GRID_SR)							:: d, square_of_sqrt2_div_2
+		real (kind = GRID_SR), DIMENSION(2,2)			:: transform
+		real (kind = GRID_SR), DIMENSION(2)				:: center_point, test1, test2
+
+		! length of each leg edge
+		d = 1.0_GRID_SR/_SWE_SIMD_ORDER
+
+		! used for computing transformations
+		square_of_sqrt2_div_2 = sqrt(2.0_GRID_SR)*0.5_GRID_SR
+		square_of_sqrt2_div_2 = square_of_sqrt2_div_2 * square_of_sqrt2_div_2
+
+		! considering that the original patch has vertices (0,0), (1,0), (0,1),
+		! the first child will be (0,0), (1,0), (sqrt(2),sqrt(2))
+		! and the second child will be (0,0), (sqrt(2),sqrt(2)), (0,1)
+
+		! === compute for first child ===
+		! transformation = rotate by (-135) degrees and scale by sqrt(2)/2
+		! sin(-135) = -sqrt(2)/2, cos(-135) = -sqrt(2)/2
+		transform_1(1,:) = [-square_of_sqrt2_div_2, +square_of_sqrt2_div_2]
+		transform_1(2,:) = [-square_of_sqrt2_div_2, -square_of_sqrt2_div_2]
+
+		! === compute for second child ===
+		! transformation = rotate by (+135) degrees and scale by sqrt(2)/2
+		! sin(+135) = sqrt(2)/2, cos(+135) = -sqrt(2)/2
+		transform_2(1,:) = [-square_of_sqrt2_div_2, -square_of_sqrt2_div_2]
+		transform_2(2,:) = [+square_of_sqrt2_div_2, -square_of_sqrt2_div_2]
+
+		do i=1, _SWE_SIMD_ORDER_SQUARE ! i = cell id in child
+			! compute baricenter of cell
+			center_point = [0.0_GRID_SR, 0.0_GRID_SR]
+			do j=1,3
+				center_point = center_point + geom%coords(:,j,i)
+			end do
+			center_point = center_point/3.0_GRID_SR
+
+			! a child cell will tipically lie with one or two cells belonging to the parent
+			! --> test a point slightly to the left and other slightly to the right and store in which
+			! 	  parent cell they lie. The result can be the same for both, but it doesn't matter.
+			test1 = center_point + [0.1_GRID_SR*d, 0.0_GRID_SR]
+			test2 = center_point + [-0.1_GRID_SR*d, 0.0_GRID_SR]
+			! apply transformation --> a translation and then the computed transform matrix
+			test1 = test1 - [1.0_GRID_SR, 0.0_GRID_SR]
+			test2 = test2 - [1.0_GRID_SR, 0.0_GRID_SR]
+			test1 = matmul(transform_1,test1)
+			test2 = matmul(transform_1,test2)
+
+			! save the relationships -> parent cells in first_child(i,:) intersect first child cell #i and so on
+			geom%first_child(i,1) = geom%cell_in_which_the_point_lies(test1)
+			geom%first_child(i,2) = geom%cell_in_which_the_point_lies(test2)
+
+			! do the same for the second child --> note that the translation is also different
+			test1 = center_point + [0.1_GRID_SR*d, 0.0_GRID_SR]
+			test2 = center_point + [-0.1_GRID_SR*d, 0.0_GRID_SR]
+			test1 = test1 - [0.0_GRID_SR, 1.0_GRID_SR]
+			test2 = test2 - [0.0_GRID_SR, 1.0_GRID_SR]
+			test1 = matmul(transform_2,test1)
+			test2 = matmul(transform_2,test2)
+
+			geom%second_child(i,1) = geom%cell_in_which_the_point_lies(test1)
+			geom%second_child(i,2) = geom%cell_in_which_the_point_lies(test2)
+		end do
+	END SUBROUTINE
+
+	integer FUNCTION cell_in_which_the_point_lies(geom,point)
+		CLASS(t_SWE_SIMD_geometry), INTENT(INOUT) 				:: geom
+		real (kind=GRID_SR), dimension(2), intent(in)			:: point
+		integer													:: i
+
+		do i=1, _SWE_SIMD_ORDER_SQUARE
+			if (is_point_inside_triangle(point, geom%coords(:,1,i), geom%coords(:,2,i), geom%coords(:,3,i)) > 0) then
+				cell_in_which_the_point_lies = i
+				exit
+			end if
+		end do
+	end FUNCTION
+
+	! returns whether a->b->c is a left turn
+	integer FUNCTION left_turn(a, b, c)
+		real (kind=GRID_SR), dimension(2), intent(in)			:: a, b, c
+		
+		if ( (b(1) - a(1)) * (c(2) - b(2)) - (b(2) - a(2)) * (c(1) - b(1)) > 0 ) then
+			left_turn = 1
+		else
+			left_turn = 0
+		end if
+		
+	end FUNCTION
+	
+	! returns whether the given point is inside the COUNTER-CLOCKWISE triangle defined by the given vertices a, b, c
+	integer FUNCTION is_point_inside_triangle(point, a, b, c)
+		IMPLICIT NONE
+		real (kind=GRID_SR), dimension(2), intent(in)			:: point, a, b, c		
+		
+		if ( left_turn(a,b,point)>0 .and. left_turn(b,c,point)>0 .and. left_turn(c,a,point)>0 ) then
+			is_point_inside_triangle = 1
+		else
+			is_point_inside_triangle = 0
+		end if
+
+	END FUNCTION
 	
 END MODULE
 #endif

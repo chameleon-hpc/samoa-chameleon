7a7b833b62e0c229f55cd357fde01f13333570eb
Chaulio Ferreira <chaulio@hotmail.com>
2016-04-08 12:19:44 +0200

    SWE_SIMD: now printing counter of cell updates

--- src/SWE/SWE.f90
+++ src/SWE/SWE.f90
@@ -435,6 +435,7 @@
 							_log_write(0, '(A, T34, F12.4, A)') " Element throughput: ", 1.0d-6 * dble(grid%stats%i_traversed_cells) * (_SWE_SIMD_ORDER_SQUARE) / t_phase, " M/s"
 							_log_write(0, '(A, T34, F12.4, A)') " Memory throughput: ", dble(grid%stats%i_traversed_memory) * (_SWE_SIMD_ORDER_SQUARE) / ((1024 * 1024 * 1024) * t_phase), " GB/s"
 							_log_write(0, '(A, T34, F12.4, A)') " Cell update throughput: ", 1.0d-6 * dble(swe%euler%stats%i_traversed_cells) * (_SWE_SIMD_ORDER_SQUARE) / t_phase, " M/s"
+							_log_write(0, '(A, T34, F12.4, A)') " #Cell updates: ", 1.0d-6 * dble(swe%euler%stats%i_traversed_cells) * (_SWE_SIMD_ORDER_SQUARE), " millions"
 							_log_write(0, '(A, T34, F12.4, A)') " Flux solver throughput: ", 1.0d-6 * dble(swe%euler%stats%i_traversed_cells) * (_SWE_SIMD_NUM_EDGES)  / t_phase, " M/s"
 #						else
 							_log_write(0, '(A, T34, F12.4, A)') " Element throughput: ", 1.0d-6 * dble(grid%stats%i_traversed_cells) / t_phase, " M/s"

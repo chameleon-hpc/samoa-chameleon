138911d22bb0f5a1128f543a6f1c67b0b500cb97
lrannabauer <lrannabauer@me.com>
2016-11-12 17:07:03 +0100

    fixed dx issue

--- runSamoa.sh
+++ runSamoa.sh
@@ -4,4 +4,10 @@ rm -r output
 mkdir output
 export OMP_NUM_THREADS=8
 
-bin/samoa_swe_nompi_noasagi_gnu -tmax 24 -dmin 11 -dmax 11 -xmloutput -tout 0.005 -drytolerance 0.00005 -stestpoints 0.5 0.0
+for i in `seq 4 20`;
+do
+
+mkdir output/output_2_unlimited_$i
+
+bin/samoa_2_unlimited -tmax 0.05 -dmin $i -dmax $i -tout 0.025 -stestpoints 0.0 0.5 -output_dir output/output_2_unlimited_$i -drytolerance 0.00000000001 -xmloutput
+done

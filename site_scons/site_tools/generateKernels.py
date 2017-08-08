e397ab6529c87498e391a2433ecd79c9aa7e1056
lrannabauer <lrannabauer@mytum.de>
2017-08-08 13:04:44 +0200

    added generate kernels to site scons

---
+++ site_scons/site_tools/generateKernels.py
@@ -0,0 +1,21 @@
+import os
+from SCons.Variables import *
+
+def generate (env, **kw):
+
+    SWE_dir="src/SWE/"
+    
+    matrices_template=SWE_dir+"SWE_dg_matrices.template"
+    matrices_out=SWE_dir+"SWE_dg_matrices.f90"
+
+    with open(matrices_template, "rt") as fin:
+        with open(matrices_out, "wt") as fout:
+            for line in fin:
+                fout.write(line.replace('SWE_DG_ORDER_TAG', str(env['swe_dg_order'])))
+
+    
+    print "generate Kernels Tool"
+    return
+
+def exists(env):
+    return True

7f83e841ddce5abbe22e08d876c632f19f53b11f
Lukas Krenz <lukas@krenz.land>
2017-09-08 19:08:04 +0200

    Support orders up to 4 again.

--- compile_all.sh
+++ compile_all.sh
@@ -6,7 +6,7 @@ unset CDPATH
 cores=$(grep -c '^processor' '/proc/cpuinfo') #Includes hyperthreading. Which should be exactly what we want.
 
 # Compile dg for all orders.
-for (( order=1; order <= 2; order+=1)) do
+for (( order=1; order <= 4; order+=1)) do
     scons config=my_conf.py swe_dg_order=${order} target=release -j ${cores} swe_scenario="linear_dam_break" exe="samoa_dg_${order}_unlimited_dam" 
     scons config=my_conf.py swe_dg_order=${order} target=release -j ${cores} swe_scenario="oscillating_lake" exe="samoa_dg_${order}_unlimited_lake" 
 done

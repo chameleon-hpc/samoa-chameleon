9d0f30e9a0b421927cb8280abbce082415076938
Christoph Honal <christoph.honal@web.de>
2020-03-01 19:00:42 +0100

    Fix invalid array index in predictor

--- src/SWE/SWE_dg_predictor.f90
+++ src/SWE/SWE_dg_predictor.f90
@@ -240,7 +240,7 @@ contains
          do i = 1,_SWE_DG_ORDER+1
             select case(edge_type)
             case(-3 ,1) !right
-               indx=_SWE_DG_DOFS-(_SWE_DG_ORDER-i+1)*(_SWE_DG_ORDER-i+2)/2 +1
+               indx=_SWE_DG_DOFS-(_SWE_DG_ORDER-i+1)*(_SWE_DG_ORDER-i+2)/2
             case(-2, 2) !mid
                indx=_SWE_DG_DOFS-(_SWE_DG_ORDER-i)*(_SWE_DG_ORDER-i+1)/2
             case(-1 ,3) !left

2dba660f316c52428f07e0426d42c0ac1c60ee16
leo.rannabauer <rannabau@in.tum.de>
2020-05-05 17:15:50 +0200

    refactored matrices template

--- src/SWE/SWE_dg_matrices.f90
+++ src/SWE/SWE_dg_matrices.f90
@@ -20,91 +20,48 @@ MODULE SWE_dg_matrices
 ! #else
 ! #define _SWE_DG_BND_INTEGRATION_POINTS _SWE_DG_ORDER+1
 ! #endif
-
 ! #define 
 
-!Conversion matrices
-#include "dg_matrices/phi_1.incl"
-#include "dg_matrices/ref1_1.incl"
-#include "dg_matrices/ref2_1.incl"
-#include "dg_matrices/coarsen_1.incl"
-#include "dg_matrices/mue_inv_1.incl"
-#include "dg_matrices/nodes_1.incl"
-!#include "dg_matrices/mue_lu_1.incl"
-!#include "dg_matrices/mue_lu_pivot_1.incl"
+!Time matrices
+#include "dg_matrices/t_m_1_5.incl"
+#include "dg_matrices/t_a_5.incl"
+#include "dg_matrices/t_k_t_10_5.incl"
+#include "dg_matrices/t_k_t_11_inv_5.incl"
+    	    
+!Space matrices
+#include "dg_matrices/s_m_5.incl"
+#include "dg_matrices/s_m_inv_5.incl"
+#include "dg_matrices/s_k_x_5.incl"
+#include "dg_matrices/s_k_y_5.incl"
 
-!New and refactored
-#include "dg_matrices/t_k_t_10_1.incl"
-#include "dg_matrices/t_k_t_11_inv_1.incl"
-#include "dg_matrices/s_m_inv_1.incl"
-#include "dg_matrices/s_k_x_1.incl"
-#include "dg_matrices/s_k_y_1.incl"
-#include "dg_matrices/s_m_1.incl"
-#include "dg_matrices/t_m_1_1.incl"
-#include "dg_matrices/t_a_1.incl"
+!Boundary matrices
+#include "dg_matrices/s_b_1_5.incl"
+#include "dg_matrices/s_b_2_5.incl"
+#include "dg_matrices/s_b_3_5.incl"
 
-#include "dg_matrices/s_b_1_1.incl"
-#include "dg_matrices/s_b_2_1.incl"
-#include "dg_matrices/s_b_3_1.incl"
+#include "dg_matrices/s_b_1_l_5.incl"
+#include "dg_matrices/s_b_2_m_5.incl"
+#include "dg_matrices/s_b_3_r_5.incl"
 
-#include "dg_matrices/s_b_1_l_1.incl"
-#include "dg_matrices/s_b_2_m_1.incl"
-#include "dg_matrices/s_b_3_r_1.incl"
+!FV projection
+#include "dg_matrices/phi_l_5.incl"
+#include "dg_matrices/phi_m_5.incl"
+#include "dg_matrices/phi_r_5.incl"
+#include "dg_matrices/phi_5.incl"
+#include "dg_matrices/mue_inv_5.incl"
 
-!New FV projection
-#include "dg_matrices/phi_l_1.incl"
-#include "dg_matrices/phi_m_1.incl"
-#include "dg_matrices/phi_r_1.incl"
+!Coordinates	    
+#include "dg_matrices/nodes_5.incl"
+#include "dg_matrices/mirrored_coords_5.incl"
 
-
-
-!old only for debugging
-#include "dg_matrices/s_k_x_o_1.incl"
-#include "dg_matrices/s_k_y_o_1.incl"
-#include "dg_matrices/s_m_o_1.incl"
-
-!DG_Solver matrices
-#include "dg_matrices/t_m_1.incl"
-#include "dg_matrices/b_m_1_1.incl"
-#include "dg_matrices/b_m_2_1.incl"
-#include "dg_matrices/b_m_3_1.incl"
-
-!#include "dg_matrices/s_m_lu_1.incl"
-!#include "dg_matrices/s_m_lu_pivot_1.incl"
-
-#include "dg_matrices/basis_der_x_1.incl"
-#include "dg_matrices/basis_der_y_1.incl"
-
-!DG_Predictor matrices
-#include "dg_matrices/st_k_x_1.incl"
-#include "dg_matrices/st_k_y_1.incl"
-
-#include "dg_matrices/st_m_1.incl"
-
-#include "dg_matrices/st_w_k_t_1_0_1.incl"
-#include "dg_matrices/st_w_k_t_1_1_inv_1.incl"
-! #include "dg_matrices/st_w_k_t_1_1_lu_1.incl"
-! #include "dg_matrices/st_w_k_t_1_1_lu_pivot_1.incl"
-
-!#include "dg_matrices/basis_der_st_x_1.incl"
-!#include "dg_matrices/basis_der_st_y_1.incl"
-
-
-!L2 projection
-! #include "dg_matrices/st_gl_node_vals_1.incl"
-! #include "dg_matrices/st_gl_weights_1.incl"
-! #include "dg_matrices/st_der_x_gl_node_vals_1.incl"
-! #include "dg_matrices/st_der_y_gl_node_vals_1.incl"
-
-! #include "dg_matrices/s_der_x_gl_node_vals_1.incl"
-! #include "dg_matrices/s_der_y_gl_node_vals_1.incl"
-
-! #include "dg_matrices/st_m_lu_1.incl"
-! #include "dg_matrices/st_m_lu_pivot_1.incl"
-
-
-! #include "dg_matrices/bnd_gl_node_vals_1.incl"
-! #include "dg_matrices/bnd_gl_weights_1.incl"
+!Basis derivatives
+#include "dg_matrices/basis_der_x_5.incl"
+#include "dg_matrices/basis_der_y_5.incl"
+	    
+!Refinement matrices
+#include "dg_matrices/ref1_5.incl"
+#include "dg_matrices/ref2_5.incl"
+#include "dg_matrices/coarsen_5.incl"
 
 real(kind=GRID_SR), Parameter :: s_m_inv_s_k_x_t   (_SWE_DG_DOFS,_SWE_DG_DOFS)     = matmul(s_m_inv,transpose(s_k_x))
 real(kind=GRID_SR), Parameter :: s_m_inv_s_k_y_t   (_SWE_DG_DOFS,_SWE_DG_DOFS)     = matmul(s_m_inv,transpose(s_k_y))

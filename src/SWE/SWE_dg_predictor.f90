04575cc6872e53f5a47b6d1ef8836e7ed1cfc472
Christoph Honal <christoph.honal@web.de>
2020-07-11 03:59:23 +0000

    Fix time-dependent boundaries

--- src/SWE/SWE_dg_predictor.f90
+++ src/SWE/SWE_dg_predictor.f90
@@ -12,6 +12,7 @@ MODULE SWE_DG_predictor
   use Samoa_swe
   use SWE_DG_Limiter
   use SWE_dg_solver
+  use SWE_PDE
 
 #if defined(CHAMELEON)
    use Chameleon_lib
@@ -33,7 +34,7 @@ MODULE SWE_DG_predictor
 # define _GT_ELEMENT_OP             element_op
 # define _GT_CELL_TO_EDGE_OP        cell_to_edge_op_dg  
   
-  public dg_predictor,flux_1,flux_2
+  public dg_predictor
   public writeFVBoundaryFields
 #		include "SFC_generic_traversal_ringbuffer.f90"
   
@@ -419,56 +420,6 @@ MODULE SWE_DG_predictor
        end associate
     end if
   end subroutine writeFVBoundaryFields
-
-function flux(q,N)
-  real(kind=GRID_SR)             :: flux(2,N,3)
-  real(kind=GRID_SR), intent(in) :: q(N,3)
-  integer                        :: N
-  
-  flux(1,:,1) = q(:,2)
-  flux(1,:,2) = q(:,2)**2/q(:,1) + 0.5_GRID_SR * g * q(:,1)**2
-  flux(1,:,3) = q(:,2)*q(:,3)/q(:,1)
-  
-  flux(2,:,1) = q(:,3)
-  flux(2,:,2) = q(:,2)*q(:,3)/q(:,1)
-  flux(2,:,3) = q(:,3)**2/q(:,1) + 0.5_GRID_SR * g * q(:,1)**2
-  
-end function flux
-  
-function flux_1(q,N)
-  real(kind=GRID_SR)             ::flux_1(N,3)
-  real(kind=GRID_SR) ,intent(in) ::q(N,3)
-  integer :: N
-  flux_1(:,1) = q(:,2)
-  flux_1(:,2) = q(:,2)**2/q(:,1) + 0.5_GRID_SR * g * q(:,1)**2
-  !flux_1(:,2) = q(:,2)**2/q(:,1)
-  flux_1(:,3) = q(:,2)*q(:,3)/q(:,1)
-end function flux_1
-
-function flux_2(q,N)
-  real(kind=GRID_SR)             ::flux_2(N,3)
-  real(kind=GRID_SR) ,intent(in) ::q(N,3)
-  integer :: N
-  flux_2(:,1) = q(:,3)
-  flux_2(:,2) = q(:,2)*q(:,3)/q(:,1)
-  flux_2(:,3) = q(:,3)**2/q(:,1) + 0.5_GRID_SR * g * q(:,1)**2
-  !flux_2(:,3) = q(:,3)**2/q(:,1)
-end function flux_2
-
-function flux_no_grav(q,N)
-  real(kind=GRID_SR)             ::flux_no_grav(2,N,3)
-  real(kind=GRID_SR) ,intent(in) ::q(N,3)
-  integer :: N
-
-  flux_no_grav(1,:,1) = q(:,2)
-  flux_no_grav(1,:,2) = q(:,2)**2/q(:,1)
-  flux_no_grav(1,:,3) = q(:,2)*q(:,3)/q(:,1)
-
-  flux_no_grav(2,:,1) = q(:,3)
-  flux_no_grav(2,:,2) = q(:,2)*q(:,3)/q(:,1)
-  flux_no_grav(2,:,3) = q(:,3)**2/q(:,1)
-
-end function flux_no_grav
 
 END MODULE SWE_DG_predictor
 

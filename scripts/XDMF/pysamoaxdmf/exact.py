dbbf63b1ed84b8d87046082bdae94a097dcc9522
Leonhard Rannabauer <di29zux4@login01.sng.lrz.de>
2021-01-21 18:43:38 +0100

    addeded 05 factor to quardature

--- scripts/XDMF/pysamoaxdmf/exact.py
+++ scripts/XDMF/pysamoaxdmf/exact.py
@@ -10,13 +10,14 @@ class ExactQuadRule:
     """This class holds the alpha optimized nodes location and weights"""
 
     def __init__(self):
-        basepath = os.path.dirname(__file__) + "/nodes/"
+        basepath = os.path.dirname(__file__) + "/exact/"
         self.configs = [None] * 10
         self.transformation = [None] * 10
         self.configs[0] = [ [1.0 / 3.0, 1.0 / 3.0, 0.5] ]
         for n in range(2, 8):
-            self.configs[n]        = np.load("exact/quad_"+str(n)+".npy")
-            self.transformation[n] = np.load("exact/transform_"+str(n)+".npy")
+            self.configs[n]        = np.load(basepath+"/quad_"+str(n)+".npy")
+            self.configs[n][:,2]   = self.configs[n][:,2] * 0.5
+            self.transformation[n] = np.load(basepath+"/transform_"+str(n)+".npy")
             # df = pd.read_csv(basepath + 'nodes_' + str(n) + '.txt', sep=' ', header=None)
             # df.columns = ['x', 'y']
             # dfw = pd.read_csv(basepath + 'weights_' + str(n) + '.txt', sep=' ', header=None)
@@ -25,6 +26,8 @@ class ExactQuadRule:
             # entries = []
             # for _, row in df.iterrows(): entries.append([row['x'], row['y'], row['w']])
             #  = entries
+        for n in range(2, 8):
+            print(sum(np.array(self.configs[n])[:,2]))
 
     def __str__(self):
         retstr = "Exact Quadrature Rule nformation:\n"
@@ -39,6 +42,3 @@ class ExactQuadRule:
 
     def dof_to_order(self, dof):
         return int(0.5 * (-3.0 + math.sqrt((8.0 * dof) + 1.0)))
-
-
-ExactQuadRule()    

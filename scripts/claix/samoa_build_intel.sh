087f957189f0d2a160626cfee9ef13700b9b781c
Jannis Klinkenberg <jannis.klinkenberg@rwth-aachen.de>
2021-10-16 01:06:23 +0200

    implemented ASAGI version

--- scripts/claix/samoa_build_intel.sh
+++ scripts/claix/samoa_build_intel.sh
@@ -1,28 +1,34 @@
 #!/usr/local_rwth/bin/zsh
 
 # parameters that can be set from outside
+export CHAMELEON_DIR=${CHAMELEON_DIR:-"/path/to/chameleon/dir"}
 export SAMOA_DIR=${SAMOA_DIR:-"/path/to/samoa/dir"}
 export ASAGI_DIR=${ASAGI_DIR:-"/path/to/asagi/dir"}
-export CHAMELEON_DIR=${CHAMELEON_DIR:-"/path/to/chameleon/dir"}
+export USE_ASAGI=${USE_ASAGI:-1}
+export SWE_SCENARIO=${SWE_SCENARIO:-"oscillating_lake"}
+# export SWE_SCENARIO=${SWE_SCENARIO:-"radial_dam_break"}
 export SAMOA_PATCH_ORDER=${SAMOA_PATCH_ORDER:-7}
 export ENABLE_TRACING_ITAC=${ENABLE_TRACING_ITAC:-0}
 export ENABLE_TRACING_EXTRAE=${ENABLE_TRACING_EXTRAE:-0}
 export COMPILE_WS=${COMPILE_WS:-0}
 export COMPILE_TASKING=${COMPILE_TASKING:-0}
 export COMPILE_CHAMELEON=${COMPILE_CHAMELEON:-0}
-export SWE_SCENARIO=${SWE_SCENARIO:-"oscillating_lake"}
-# export SWE_SCENARIO=${SWE_SCENARIO:-"radial_dam_break"}
+export COMPILE_PACKING=${COMPILE_PACKING:-0}
+
 
 echo ">>> Building with the following config:"
+echo "CHAMELEON_DIR: ${CHAMELEON_DIR}"
 echo "SAMOA_DIR: ${SAMOA_DIR}"
 echo "ASAGI_DIR: ${ASAGI_DIR}"
+echo "USE_ASAGI: ${USE_ASAGI}"
+echo "SWE_SCENARIO: ${SWE_SCENARIO}"
 echo "SAMOA_PATCH_ORDER: ${SAMOA_PATCH_ORDER}"
 echo "ENABLE_TRACING_ITAC: ${ENABLE_TRACING_ITAC}"
 echo "ENABLE_TRACING_EXTRAE: ${ENABLE_TRACING_EXTRAE}"
 echo "COMPILE_WS: ${COMPILE_WS}"
 echo "COMPILE_TASKING: ${COMPILE_TASKING}"
 echo "COMPILE_CHAMELEON: ${COMPILE_CHAMELEON}"
-echo "SWE_SCENARIO: ${SWE_SCENARIO}"
+echo "COMPILE_PACKING: ${COMPILE_PACKING}"
 
 # remember current working directory
 CUR_DIR=$(pwd)
@@ -48,17 +54,33 @@ cd ${SAMOA_DIR}
 
 # ========== synthetic scenario: oscillating_lake ==========
 if [ "${COMPILE_WS}" = "1" ]; then
-    scons asagi=off target=release openmp=notasks trace_extrae=${ENABLE_TRACING_EXTRAE} trace_itac=${ENABLE_TRACING_ITAC} scenario=swe swe_patch_order=${SAMOA_PATCH_ORDER} data_refinement=sample swe_scenario=${SWE_SCENARIO} flux_solver=aug_riemann assertions=on compiler=intel mpi=intel -j8
+    scons target=release openmp=notasks \
+        asagi=${USE_ASAGI} asagi_dir=${ASAGI_DIR} \
+        trace_extrae=${ENABLE_TRACING_EXTRAE} trace_itac=${ENABLE_TRACING_ITAC} \
+        scenario=swe swe_patch_order=${SAMOA_PATCH_ORDER} swe_scenario=${SWE_SCENARIO} \
+        data_refinement=sample flux_solver=aug_riemann assertions=on compiler=intel mpi=intel -j8
 fi
 if [ "${COMPILE_TASKING}" = "1" ]; then
-    scons asagi=off target=release openmp=tasks   trace_extrae=${ENABLE_TRACING_EXTRAE} trace_itac=${ENABLE_TRACING_ITAC} scenario=swe swe_patch_order=${SAMOA_PATCH_ORDER} data_refinement=sample swe_scenario=${SWE_SCENARIO} flux_solver=aug_riemann assertions=on compiler=intel mpi=intel -j8
+    scons target=release openmp=tasks \
+        asagi=${USE_ASAGI} asagi_dir=${ASAGI_DIR} \
+        trace_extrae=${ENABLE_TRACING_EXTRAE} trace_itac=${ENABLE_TRACING_ITAC} \
+        scenario=swe swe_patch_order=${SAMOA_PATCH_ORDER} swe_scenario=${SWE_SCENARIO} \
+        data_refinement=sample flux_solver=aug_riemann assertions=on compiler=intel mpi=intel -j8
+fi
+if [ "${COMPILE_PACKING}" = "1" ]; then
+    scons target=release chameleon=2 chameleon_dir=${CHAMELEON_DIR} \
+        asagi=${USE_ASAGI} asagi_dir=${ASAGI_DIR} \
+        trace_extrae=${ENABLE_TRACING_EXTRAE} trace_itac=${ENABLE_TRACING_ITAC} \
+        scenario=swe swe_patch_order=${SAMOA_PATCH_ORDER} swe_scenario=${SWE_SCENARIO} \
+        data_refinement=sample flux_solver=aug_riemann assertions=on compiler=intel mpi=intel -j8
 fi
 if [ "${COMPILE_CHAMELEON}" = "1" ]; then
-    scons asagi=off target=release chameleon=1 chameleon_dir=${CHAMELEON_DIR} trace_extrae=${ENABLE_TRACING_EXTRAE} trace_itac=${ENABLE_TRACING_ITAC} scenario=swe swe_patch_order=${SAMOA_PATCH_ORDER} data_refinement=sample swe_scenario=${SWE_SCENARIO} flux_solver=aug_riemann assertions=on compiler=intel mpi=intel -j8
+    scons target=release chameleon=1 chameleon_dir=${CHAMELEON_DIR} \
+        asagi=${USE_ASAGI} asagi_dir=${ASAGI_DIR} \
+        trace_extrae=${ENABLE_TRACING_EXTRAE} trace_itac=${ENABLE_TRACING_ITAC} \
+        scenario=swe swe_patch_order=${SAMOA_PATCH_ORDER} swe_scenario=${SWE_SCENARIO} \
+        data_refinement=sample flux_solver=aug_riemann assertions=on compiler=intel mpi=intel -j8
 fi
-
-# ========== ASAGI version ==========
-# TODO:
 
 # switch back to working directory
 cd ${CUR_DIR}
--- scripts/claix/samoa_run_intel.sh
+++ scripts/claix/samoa_run_intel.sh
@@ -2,34 +2,41 @@
 #SBATCH --time=01:00:00
 #SBATCH --partition=c18m
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
-#SBATCH --cpus-per-task=24
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=12
 #SBATCH --exclusive
 #SBATCH --job-name=samoa-osc
 #SBATCH --output=samoa-osc.%J.txt
 
 # parameters that can be set from outside
-export SAMOA_DIR=${SAMOA_DIR:-"/path/to/samoa/dir"}
 export OUTPUT_DIR=${OUTPUT_DIR:-"/path/to/samoa/dir"}
+export SAMOA_DIR=${SAMOA_DIR:-"/path/to/samoa/dir"}
+export ASAGI_DATA_DIR=${ASAGI_DATA_DIR:-"/path/to/asagi/data/dir"}
+export USE_ASAGI=${USE_ASAGI:-1}
 export ENABLE_TRACING_ITAC=${ENABLE_TRACING_ITAC:-0}
 export ENABLE_TRACING_EXTRAE=${ENABLE_TRACING_EXTRAE:-0}
 export ENABLE_PROFILING=${ENABLE_PROFILING:-0}
 export RUN_WS=${RUN_WS:-0}
 export RUN_TASKING=${RUN_TASKING:-0}
 export RUN_CHAMELEON=${RUN_CHAMELEON:-0}
+export RUN_PACKING=${RUN_PACKING:-0}
 export SWE_SCENARIO=${SWE_SCENARIO:-"oscillating_lake"}
 # export SWE_SCENARIO=${SWE_SCENARIO:-"radial_dam_break"}
 export NUM_RANKS=${NUM_RANKS:-2}
 export OMP_NUM_THREADS=${OMP_NUM_THREADS:-11}
 
 echo ">>> Running with the following config:"
-echo "SAMOA_DIR: ${SAMOA_DIR}"
 echo "OUTPUT_DIR: ${OUTPUT_DIR}"
+echo "SAMOA_DIR: ${SAMOA_DIR}"
+echo "ASAGI_DATA_DIR: ${ASAGI_DATA_DIR}"
+echo "USE_ASAGI: ${USE_ASAGI}"
 echo "ENABLE_TRACING_ITAC: ${ENABLE_TRACING_ITAC}"
 echo "ENABLE_TRACING_EXTRAE: ${ENABLE_TRACING_EXTRAE}"
+echo "ENABLE_PROFILING: ${ENABLE_PROFILING}"
 echo "RUN_WS: ${RUN_WS}"
 echo "RUN_TASKING: ${RUN_TASKING}"
 echo "RUN_CHAMELEON: ${RUN_CHAMELEON}"
+echo "RUN_PACKING: ${RUN_PACKING}"
 echo "SWE_SCENARIO: ${SWE_SCENARIO}"
 echo "NUM_RANKS: ${NUM_RANKS}"
 echo "OMP_NUM_THREADS: ${OMP_NUM_THREADS}"
@@ -42,7 +49,6 @@ fi
 if [ "${ENABLE_TRACING_EXTRAE}" = "1" ]; then
     module load DEV-TOOLS extrae papi
     export EXTRAE_CONFIG_FILE=$(pwd)/extrae.xml
-    #export CMD_EXTRAE_PREFIX="$(pwd)/trace.sh"
     export LIBRARY_PATH="$LIBRARY_PATH:${EXTRAE_HOME}/lib"
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${EXTRAE_HOME}/lib"
 fi
@@ -53,6 +59,10 @@ if [[ "${RUN_CHAMELEON}" == "1" ]]; then
     # Append linux env vars with Chameleon include and lib folder (here: realized using an environment module)
     source ~/.zshrc
     module load chameleon
+fi
+if [[ "${USE_ASAGI}" == "0" ]]; then
+    export ASAGI_NAME_EXT="_noasagi"
+    export SWE_NAME_EXT="${SWE_SCENARIO}_"
 fi
 
 # create temporary output directory
@@ -78,72 +88,64 @@ export DMIN=15
 export DMAX=23
 export NUM_SECTIONS=16
 export NUM_STEPS=100
-export SIM_TIME_SEC=10
+export SIM_TIME_SEC=600
 export LB_FREQ="-lbfreq 1000000"
-export SIM_LIMIT="-nmax ${NUM_STEPS}"
-#export SIM_LIMIT="-tmax ${SIM_TIME_SEC}"
+export ASAGI_PARAMS="-fbath ${ASAGI_DATA_DIR}/bath.nc -fdispl ${ASAGI_DATA_DIR}/displ.nc"
+# export SIM_LIMIT="-nmax ${NUM_STEPS}"
+export SIM_LIMIT="-tmax ${SIM_TIME_SEC}"
 export OUTPUT_PARAMS=""
 #export OUTPUT_PARAMS="-nout 10 -xmloutput"
 
 # final parameters
-export SAMOA_PARAMS="-output_dir ${OUTPUT_DIR} -dmin ${DMIN} -dmax ${DMAX} -sections ${NUM_SECTIONS} ${SIM_LIMIT} ${OUTPUT_PARAMS} ${LB_FREQ}"
+export SAMOA_PARAMS="-output_dir ${OUTPUT_DIR} -dmin ${DMIN} -dmax ${DMAX} -sections ${NUM_SECTIONS} ${ASAGI_PARAMS} ${SIM_LIMIT} ${OUTPUT_PARAMS} ${LB_FREQ}"
 
-# ========== application runs: oscillating lake (work-sharing) ==========
-if [[ "${RUN_WS}" == "1" ]]; then
+run_experiment() {
     if [[ "${ENABLE_TRACING_ITAC}" == "1" ]]; then
-        mkdir -p ${CUR_DIR}/trace_osc_ws
-        export VT_LOGFILE_PREFIX=${CUR_DIR}/trace_osc_ws
+        mkdir -p ${CUR_DIR}/trace_itac_${NAME_EXTENSION}
+        export VT_LOGFILE_PREFIX=${CUR_DIR}/trace_itac_${NAME_EXTENSION}
     fi
     if [[ "${ENABLE_TRACING_EXTRAE}" == "1" ]]; then
-        TMP_EXTRAE="${CUR_DIR}/trace_extrae_osc_ws"
+        TMP_EXTRAE="${CUR_DIR}/trace_extrae_${NAME_EXTENSION}"
         mkdir -p ${TMP_EXTRAE}
         cd ${TMP_EXTRAE}
     fi
     if [[ "${ENABLE_PROFILING}" == "1" ]]; then
-        export CMD_VTUNE_PREFIX="amplxe-cl –collect hotspots –r ${CUR_DIR}/profile_osc_ws_${OMP_NUM_THREADS}t -trace-mpi -- "
+        export CMD_VTUNE_PREFIX="amplxe-cl –collect hotspots –r ${CUR_DIR}/profile_${NAME_EXTENSION}_${OMP_NUM_THREADS}t -trace-mpi -- "
     fi
-    ${CUR_MPI_CMD} ${CMD_VTUNE_PREFIX} ${CMD_EXTRAE_PREFIX} ${SAMOA_DIR}/bin/samoa_swe_${SWE_SCENARIO}_notasks_intel_noasagi ${SAMOA_PARAMS} &> ${CUR_DIR}/log_samoa_run_osc_ws.log
+    ${CUR_MPI_CMD} ${CMD_VTUNE_PREFIX} ${CMD_EXTRAE_PREFIX} ${SAMOA_DIR}/bin/${EXE_NAME} ${SAMOA_PARAMS} &> ${CUR_DIR}/log_samoa_run_${NAME_EXTENSION}.log
     if [[ "${ENABLE_TRACING_EXTRAE}" == "1" ]]; then
         mpi2prv -f TRACE.mpits -v -paraver
     fi
+}
+
+# ========== application runs: oscillating lake (work-sharing) ==========
+if [[ "${RUN_WS}" == "1" ]]; then
+    echo "Running work-sharing version"
+    export EXE_NAME=samoa_swe_${SWE_NAME_EXT}notasks_intel${ASAGI_NAME_EXT}
+    export NAME_EXTENSION=ws
+    run_experiment
 fi
 
 # ========== application runs: oscillating lake (tasking) ==========
 if [[ "${RUN_TASKING}" == "1" ]]; then
-    if [[ "${ENABLE_TRACING_ITAC}" == "1" ]]; then
-        mkdir -p ${CUR_DIR}/trace_osc_tasking
-        export VT_LOGFILE_PREFIX=${CUR_DIR}/trace_osc_tasking
-    fi
-    if [[ "${ENABLE_TRACING_EXTRAE}" == "1" ]]; then
-        TMP_EXTRAE="${CUR_DIR}/trace_extrae_osc_tasking"
-        mkdir -p ${TMP_EXTRAE}
-        cd ${TMP_EXTRAE}
-    fi
-    if [[ "${ENABLE_PROFILING}" == "1" ]]; then
-        export CMD_VTUNE_PREFIX="amplxe-cl –collect hotspots –r ${CUR_DIR}/profile_osc_tasking_${OMP_NUM_THREADS}t -trace-mpi -- "
-    fi
-    ${CUR_MPI_CMD} ${CMD_VTUNE_PREFIX} ${CMD_EXTRAE_PREFIX} ${SAMOA_DIR}/bin/samoa_swe_${SWE_SCENARIO}_intel_noasagi ${SAMOA_PARAMS} &> ${CUR_DIR}/log_samoa_run_osc_tasks.log
-    if [[ "${ENABLE_TRACING_EXTRAE}" == "1" ]]; then
-        mpi2prv -f TRACE.mpits -v -paraver
-    fi
+    echo "Running tasking version"
+    export EXE_NAME=samoa_swe_${SWE_NAME_EXT}intel${ASAGI_NAME_EXT}
+    export NAME_EXTENSION=tasking
+    run_experiment
+fi
+
+# ========== application runs: oscillating lake (packing = OpenMP tasks) ==========
+if [[ "${RUN_PACKING}" == "1" ]]; then
+    echo "Running packing version"
+    export EXE_NAME=samoa_swe_${SWE_NAME_EXT}intel${ASAGI_NAME_EXT}_packing
+    export NAME_EXTENSION=packing
+    run_experiment
 fi
 
 # ========== application runs: oscillating lake (chameleon) ==========
 if [[ "${RUN_CHAMELEON}" == "1" ]]; then
-    if [[ "${ENABLE_TRACING_ITAC}" == "1" ]]; then
-        mkdir -p ${CUR_DIR}/trace_osc_chameleon
-        export VT_LOGFILE_PREFIX=${CUR_DIR}/trace_osc_chameleon
-    fi
-    if [[ "${ENABLE_TRACING_EXTRAE}" == "1" ]]; then
-        TMP_EXTRAE="${CUR_DIR}/trace_extrae_osc_chameleon"
-        mkdir -p ${TMP_EXTRAE}
-        cd ${TMP_EXTRAE}
-    fi
-    if [[ "${ENABLE_PROFILING}" == "1" ]]; then
-        export CMD_VTUNE_PREFIX="amplxe-cl –collect hotspots –r ${CUR_DIR}/profile_osc_chameleon_${OMP_NUM_THREADS}t -trace-mpi -- "
-    fi
-    ${CUR_MPI_CMD} ${CMD_VTUNE_PREFIX} ${CMD_EXTRAE_PREFIX} ${SAMOA_DIR}/bin/samoa_swe_${SWE_SCENARIO}_intel_noasagi_chameleon ${SAMOA_PARAMS} &> ${CUR_DIR}/log_samoa_run_osc_chameleon.log
-    if [[ "${ENABLE_TRACING_EXTRAE}" == "1" ]]; then
-        mpi2prv -f TRACE.mpits -v -paraver
-    fi
+    echo "Running Chameleon version"
+    export EXE_NAME=samoa_swe_${SWE_NAME_EXT}intel${ASAGI_NAME_EXT}_chameleon
+    export NAME_EXTENSION=chameleon
+    run_experiment
 fi
